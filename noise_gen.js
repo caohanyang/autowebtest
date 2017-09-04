2b8d96ea130823b3fab488537c4f884767def127
xurongfeibuaa <xurongfeik@163.com>
2017-09-04 18:38:20 +0200

    modify

--- noise_gen.js
+++ noise_gen.js
@@ -1,6 +1,7 @@
-var Nightmare = require('nightmare');	
+var Nightmare = require('nightmare'); 
 var nightmare = Nightmare({ show: true });
 const wat_action = require('wat-action');
+const fs = require('fs');
 
 var scenarioString = require('./scenario.json');
 const scenario = new wat_action.Scenario(scenarioString)
@@ -25,8 +26,24 @@ for(var i = 0; i < 5; i++)
       {scenario.actions.splice(insertLocation,0,scrollToAction);}
     }
 
-console.log(JSON.stringify(scenario, null, 2));
+// console.log(JSON.stringify(scenario, null, 2));
 
+var scenarioJson = JSON.stringify(JSON.parse(scenario.toJSON()),null,2);
 
+  console.log(scenarioJson);
+
+  var path = __dirname + '/scenario_noise.json';
+
+  if(fs.existsSync(path) ){
+    fs.unlinkSync(path);
+  }
+
+  fs.writeFile(path, scenarioJson, {flag: 'a'}, function (err) {
+    if(err) {
+      console.error(err);
+    } else {
+      console.log('write sucess');
+    }
+  });
 
 
