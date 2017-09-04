5a0021c2e6dd1a6c5de91c96c7b18be39551cae2
xurongfeibuaa <xurongfeik@163.com>
2017-09-04 18:22:19 +0200

    modify

--- noise_gen.js
+++ noise_gen.js
@@ -1,77 +1,32 @@
 var Nightmare = require('nightmare');	
 var nightmare = Nightmare({ show: true });
-var JSON = require('parse-json'); 
-var fs = require('fs');
-var data = fs.readFileSync('test.json','UTF-8');
-console.log(data);
-var positions = new Array();
-positions.push(0);
-var sstr = new Array();
-var noiseStr = "";
-var noiseData= "";
+const wat_action = require('wat-action');
 
-function AddNoise(){
-    SearchSubStr(data,"\"type\"");
-    positions.push(data.length);
-    for (var i = 0; i < positions.length-1; i++)
-      {
-        sstr[i] = data.substring(positions[i]-1,positions[i+1]-1);
-      }
-      sstr[sstr.length-1] = sstr[sstr.length-1] +",";
-    for (var i = 1; i < sstr.length; i++)
-      {
-        sstr[i] = sstr[i] + GenerateNoise();
-      }
-    for (var i = 1; i < sstr.length; i++)
-      {
-        noiseData = noiseData + sstr[i];
-      }
-      noiseData = "[" + noiseData.substring(0,noiseData.length-1) + "]";
-  }
+var scenarioString = require('./scenario.json');
+const scenario = new wat_action.Scenario(scenarioString)
+const waitAction = new wat_action.WaitAction('5');
+const scrollToAction = new wat_action.ScrollToAction('100', '200');
 
-function GenerateNoise(){
-    var rd = Math.round(Math.random()*5);
-    for(var i = 0; i < rd; i++){
-      var r = Math.round(Math.random()*10);
-      if (r < 5) {noiseStr = noiseStr + AddWait();}
-      else {noiseStr = noiseStr + AddScrollTo();}
 
-    }
-    return noiseStr;
-  }
 
-function SearchSubStr(str,subStr){
-    var pos = str.indexOf(subStr);
-    while(pos>-1){
-        positions.push(pos);
-        pos = str.indexOf(subStr,pos+1);
-    }
+function RandomNumBoth(Min,Max){
+      var Range = Max - Min;
+      var Rand = Math.random();
+      var num = Min + Math.round(Rand * Range); 
+      return num;
 }
 
- function AddWait(){
+for(var i = 0; i < 5; i++)
+    {
+    var insertLocation = RandomNumBoth(1,scenario.actions.length-1);
+    if(Math.random() < 0.5)
+      {scenario.actions.splice(insertLocation,0,waitAction);}
+    else
+      {scenario.actions.splice(insertLocation,0,scrollToAction);}
+    }
 
-    var ms = Math.round(Math.random()*100);  
-    var addStr = "{\"type\":\"Wait\",\"ms\":\"" + ms + "\"},"
-    return addStr;
-
-  } 
-
- function AddScrollTo(){
-
-    var x = Math.round(Math.random()*100);  
-    var y = Math.round(Math.random()*100); 
-    var addStr = "{\"type\":\"scrollTo\",\"top\":\"" + x + "\",\"left\":\""+ y +"\"},"
-    return addStr;
-
-  } 
+console.log(JSON.stringify(scenario, null, 2));
 
 
-AddNoise();
-var obj = eval('(' + data + ')');
-console.log(obj);
-console.log("--nosie generate----------------");
-console.log(noiseData);
-var obj = eval('(' + noiseData + ')');
-console.log(obj);
 
 
