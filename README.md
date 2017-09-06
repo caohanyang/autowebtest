2766a068273e10b2a248f993c39d4ff90446658b
caohanyang <caohanyang007@gmail.com>
2017-09-06 16:31:28 +0200

    update player.js to support console arguments

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # AutoWebTest
 Auto test for the website
+
+
+Record
+-------
+cd nightdream
+make
+
+Then install chrome plugin to record Scenarios.
+You can save Scenario as scenario.json in the current path.
+
+
+Play
+-------
+node player.js [filename]
+
+example: node player.js scenario.json
--- player.js
+++ player.js
@@ -1,9 +1,16 @@
 const Nightmare = require('nightmare');	
 const nightmare = new Nightmare({show:true});
 const wat_action = require('wat-action');
+const fs = require('fs')
+const path = require('path')
 
+var scenarioString
 
-var scenarioString = require('./scenario.json');
+if (process.argv.length <= 2) {
+	scenarioString = require('./scenario.json')
+} else {
+	scenarioString = JSON.parse(fs.readFileSync(path.resolve(__dirname, process.argv[2])).toString())
+}
 const scenario = new wat_action.Scenario(scenarioString)
 
 scenario.attachTo(nightmare)
