f13f7cadff9d83516685dc829c4c6b517b10409c
chenweiLabri <888chenwei666@gmail.com>
2017-10-12 17:06:59 +0200

    change to three selectors

--- nightdream/src/content-scripts/index.js
+++ nightdream/src/content-scripts/index.js
@@ -1,5 +1,6 @@
 import cssPath from 'css-path'
 import each from 'component-each'
+import { select } from 'optimal-select';
 
 detect('click')
 detect('keydown')
@@ -37,21 +38,70 @@ function handle (event, node) {
   }
 };
 
+// function computeSelector(el) {
+//   var names = [];
+//   while (el.parentNode) {
+//     if (el.id) {
+//       names.unshift(`#${el.id}`);
+//       break;
+//     } else {
+//       if (el == el.ownerDocument.documentElement)
+//         names.unshift(el.tagName);
+//       else {
+//         for (var c = 1, e = el; e.previousElementSibling; e = e.previousElementSibling, c++);
+//           names.unshift(`${el.tagName}:nth-child(${c})`);
+//       }
+//       el = el.parentNode;
+//     }
+//   }
+//   return names.join(' > ');
+// }
+
 function computeSelector(el) {
-        var names = [];
-        while (el.parentNode) {
-            if (el.id) {
-                names.unshift(`#${el.id}`);
-                break;
-            } else {
-                if (el == el.ownerDocument.documentElement)
-                    names.unshift(el.tagName);
-                else {
-                    for (var c = 1, e = el; e.previousElementSibling; e = e.previousElementSibling, c++);
-                    names.unshift(`${el.tagName}:nth-child(${c})`);
-                }
-                el = el.parentNode;
-            }
-        }
-        return names.join(' > ');
+  return {
+      watId: computeSelectorWithID(el),
+      watPath: computeSelectorWithPath(el),
+      optimal: computeSelectorOptimal(el)
+  };
+}
+
+function computeSelectorWithID(el) {
+  var names = [];
+  while (el.parentNode) {
+    if (el.id) {
+      names.unshift(`#${el.id}`);
+      break;
+    } else {
+      if (el == el.ownerDocument.documentElement)
+        names.unshift(el.tagName);
+      else {
+        for (var c = 1, e = el; e.previousElementSibling; e = e.previousElementSibling, c++);
+          names.unshift(`${el.tagName}:nth-child(${c})`);
+      }
+      el = el.parentNode;
     }
+  }
+  return names.join(' > ');
+}
+
+function computeSelectorWithPath(el) {
+  var names = [];
+  while (el.parentNode) {
+    if (el == el.ownerDocument.documentElement)
+      names.unshift(el.tagName);
+    else {
+      for (var c = 1, e = el; e.previousElementSibling; e = e.previousElementSibling, c++);
+        names.unshift(`${el.tagName}:nth-child(${c})`);
+    }
+    el = el.parentNode;
+  }
+  return names.join(' > ');
+}
+
+function computeSelectorOptimal(el) {
+  return select(el);
+}
+
+
+
+
